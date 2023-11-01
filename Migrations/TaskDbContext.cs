afc61bc3775b57b1b12f40c090a8d06094ceedee
code-jude <judeprashanth@outlook.com>
2023-11-01 12:43:49 -0700

    First Push

---
+++ Migrations/TaskDbContext.cs
@@ -0,0 +1,25 @@
+using task_api.Models;
+using Microsoft.EntityFrameworkCore;
+using Task = task_api.Models.Task;
+
+namespace l10_rest.Migrations;
+
+public class TaskDbContext : DbContext
+{
+    public DbSet<Task> Tasks { get; set; }
+    public TaskDbContext(DbContextOptions<TaskDbContext> options)
+    : base(options)
+    {
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Task>(entity =>
+        {
+            entity.HasKey(e => e.TaskId);
+            entity.Property(e => e.Title).IsRequired();
+            entity.Property(e => e.Completed).IsRequired();
+        });
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace task_api.Models;
+
+public class Task 
+{
+    public int TaskId { get; set; }
+    [Required]
+    public string? Title { get; set; }
+
+    public bool Completed { get; set; }
+    
+}
